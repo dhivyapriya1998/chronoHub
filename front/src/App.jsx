92eee78bdb5b89edef7883e67954626d8115ad82
dhivya <dhivya.ravi98@gmail.com>
2025-07-08 00:13:03 +0530

    Added route

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import EmployeeList from "./components/Employee/EmployeeList.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 
 const App = () => (
     <Router>
-        <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/employees" element={<EmployeeList />} />
-            {/* Add more routes later */}
-        </Routes>
+        <div style={{ padding: 10 }}>
+            <nav style={{ marginBottom: 5 }}>
+                <Link to="/" style={{ marginRight: 10 }}>Dashboard</Link>
+                <Link to="/employees">Employees</Link>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/employees" element={<EmployeeList />} />
+            </Routes>
+        </div>
     </Router>
 );
 
